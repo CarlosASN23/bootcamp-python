e56c8362c0bdc95b685899a612fa0135279a489b
Carlos Alberto <128325902+Krathor23@users.noreply.github.com>
2023-07-28 20:17:21 -0300

    Add files via upload

---
+++ desafio_intermediario01.py
@@ -0,0 +1,15 @@
+C = int(input()) # Inicialização da variavel do numero de testes
+
+for i in range (C): #Inicialização do bloco para realização dos testes
+    
+    i = int(input()) # Entrada da variavel que representa o nivel de energia
+    
+    if i > 8000: # Inicialização do bloco para verificar se tem mais de 8000 de energia ou não
+
+        print("Mais de 8000!")
+
+    else:
+        print("Inseto")
+
+C = C -1 
+
