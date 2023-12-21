ddebf1f51d0b90d3f6a757039694b989610e3da0
Carlos Alberto <128325902+CarlosASN23@users.noreply.github.com>
2023-12-21 16:51:35 -0300

    Update desafio_inicial_sistema_bancario.py

--- desafio_inicial_sistema_bancario.py
+++ desafio_inicial_sistema_bancario.py
@@ -1,8 +1,8 @@
-# Sistema-bancario-Python
-#Sistema de deposito - saque - extrato com Python
+"""Sistema-bancario-Python
+   Sistema de deposito - saque - extrato com Python"""
 
 #Criação do menu inicial - Sistema bancário
-Menu = """"
+MENU = """"
 [1]:Deposito
 [2]:Saque
 [3]:Extrato
@@ -11,26 +11,27 @@ Menu = """"
 => """
 
 # inicialização das variaveis
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-limite_saques = 3
-deposito = 0
+SALDO = 0
+LIMITE = 500
+EXTRATO = ""
+NUMERO_SAQUES = 0
+LIMITE_SAQUES = 3
+DEPOSITO = 0
 
 # Calculo e operações
 
 while True:
 
-    opcao = input(Menu)
+    opcao = input(MENU)
 
     if opcao == "1": #bloco para o calculo da opção de deposito
 
         deposito= float(input("Informe o valor do deposito: "))
 
-        if deposito>0: #Verificação se o valor do deposito é maior que 0 
-            saldo += deposito #acumulador dos valores
-            extrato += f"Deposito: R$ {deposito:.2f}\n" #bloco para armazenar dado que será gerado no extrato
+        if deposito>0: #Verificação se o valor do deposito é maior que 0
+            SALDO += deposito #acumulador dos valores
+            EXTRATO += f"Deposito: R$ {deposito:.2f}\n"
+            #bloco para armazenar dado que será gerado no extrato
 
         else:
             print("Operação falhou: valor informado inválido!") #bloco para a opção de erro
@@ -44,24 +45,24 @@ while True:
 
         elif saque>500: #bloco de opção de erro ao tentar saques maiores que R$500,00
             print("Operação falhou: Valor informado excede o valor de saque disponivel")
-
-        elif deposito < saque: #bloco de opção de erro ao tentar sacar valores que excedem o saldo disponivel
+        #bloco de opção de erro ao tentar sacar valores que excedem o saldo disponivel
+        elif deposito < saque:
             print("Operação falhou: Valor informado excede o saldo disponivel")
 
-        elif numero_saques >=3: #bloco de opção de erro ao tentar sacar mais de 3x ao dia
+        elif NUMERO_SAQUES >=3: #bloco de opção de erro ao tentar sacar mais de 3x ao dia
             print("Operação falhou: Numero maximo de saques excedido")
 
         elif saque >0: #bloco para o calculo do saque e resultado do extrato
-            saldo -= saque
-            extrato += f"Saque R$ {saque:.2f}\n"
-            numero_saques += 1
+            SALDO -= saque
+            EXTRATO += f"Saque R$ {saque:.2f}\n"
+            NUMERO_SAQUES += 1
         else:
             print("Operação falhou: O valor informado é invalido!")
 
     elif opcao=="3": #bloco para a realização da operação de extrato
         print("\n ======== Extrato ========")
-        print("Não foram realizadas movimentações" if not extrato else extrato)
-        print(f"\n Saldo: R$ {saldo:.2f}")
+        print("Não foram realizadas movimentações" if not EXTRATO else EXTRATO)
+        print(f"\n Saldo: R$ {SALDO:.2f}")
         print("\n ============================")
 
     elif opcao=="4":
